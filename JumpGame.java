4d31bf6856e6597f8066020c0c7c780c7dfe309d
ATISHEY JAIN <atisheyjain97@gmail.com>
2020-05-06 15:41:42 +0530

    Update JumpGame.java

--- JumpGame.java
+++ JumpGame.java
@@ -30,3 +30,18 @@ class Solution {
     }
     
 }
+
+
+//O(n) solution. greedy approach. 
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int lastPos = n-1;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]+i >= lastPos)
+               lastPos = i;    
+        }
+        return lastPos == 0;
+    }
+}
