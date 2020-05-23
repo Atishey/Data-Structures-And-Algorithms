ac5c636b571711e47763060df63b65d5fca79674
Atishey Jain <atisheyjain97@gmail.com>
2020-05-23 21:56:14 +0530

    https://practice.geeksforgeeks.org/problems/coin-change/0

--- Dp/coinChange.java
+++ Dp/coinChange.java
@@ -30,3 +30,32 @@
 	        t--;
 	    }
 	 }
+
+//Space optimised bottom up approach
+
+static int a[],dp[];
+    
+public static void main (String[] args) throws IOException {
+   FastReader sc = new FastReader();
+   int t = sc.nextInt();
+   while(t>0) {
+	int m = sc.nextInt();
+	a = new int[m];
+
+	for(int i=0;i<m;i++){
+	    a[i] = sc.nextInt();
+	}
+	int n = sc.nextInt();
+	//Array storing no. of ways to get value n when m coins are given.
+	dp = new int[n+1];
+	dp[0] = 1;
+
+	for(int i=0;i<m;i++){
+	    for(int j=a[i];j<=n;j++){
+	    dp[j] += dp[j-a[i]];
+	    }
+	} 
+	System.out.println(dp[n]);
+	t--;
+    }
+ }
